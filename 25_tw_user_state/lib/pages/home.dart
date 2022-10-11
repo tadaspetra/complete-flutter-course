3281e6959e22f0a6e594db13e2d365de6ad455f8
Tadas Petra <tadas@tadaspetra.com>
2022-10-11 14:07:29 -0500

    Added user state controller

---
+++ 25_tw_user_state/lib/pages/home.dart
@@ -0,0 +1,37 @@
+import 'package:context/controllers/user_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Home extends ConsumerWidget {
+  Home({super.key});
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    LocalUser currentUser = ref.watch(usersProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await _auth.signOut();
+              ref.read(usersProvider.notifier).logOut();
+            },
+            child: const Text(
+              "Sign Out",
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Text(currentUser.id),
+          Text(currentUser.user.email),
+        ],
+      ),
+    );
+  }
+}
