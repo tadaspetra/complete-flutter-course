0e23b70b092048bb997733fcfd3a713ef628ed2f
Tadas Petra <tadas@tadaspetra.com>
2022-11-25 14:54:57 -0600

    Updated after recording

--- 30_tw_writing_a_tweet/lib/pages/home.dart
+++ 30_tw_writing_a_tweet/lib/pages/home.dart
@@ -1,80 +1,72 @@
-import 'package:context/controllers/user_state.dart';
 import 'package:context/pages/create.dart';
 import 'package:context/pages/settings.dart';
+import 'package:context/providers/user_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Home extends ConsumerWidget {
-  Home({super.key});
+  const Home({super.key});
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    LocalUser currentUser = ref.watch(usersProvider);
+    LocalUser currentUser = ref.watch(userProvider);
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Home"),
         leading: Builder(builder: (context) {
           return GestureDetector(
             onTap: () => Scaffold.of(context).openDrawer(),
-            child: Container(
-              padding: const EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
               child: CircleAvatar(
-                foregroundImage: NetworkImage(currentUser.user.profilePic),
-                onForegroundImageError: (exception, stackTrace) {},
+                backgroundImage: NetworkImage(
+                  currentUser.user.profilePic,
+                ),
               ),
             ),
           );
         }),
-        title: const Text("Home"),
       ),
-      body: Column(
+      body: Column(children: [
+        Text(currentUser.user.email),
+        Text(currentUser.user.name),
+      ]),
+      drawer: Drawer(
+          child: Column(
         children: [
-          Text(currentUser.user.email),
-          Text(currentUser.user.name),
+          Image.network(
+            currentUser.user.profilePic,
+          ),
+          ListTile(
+            title: Text(
+              "Hello, ${currentUser.user.name}",
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ),
+          ListTile(
+            title: const Text("Settings"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const Settings()));
+            },
+          ),
+          ListTile(
+            title: const Text("Sign Out"),
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+              ref.read(userProvider.notifier).logout();
+            },
+          )
         ],
-      ),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => CreateTweet(),
-          ));
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: ((context) => const CreateTweet())));
         },
         child: const Icon(Icons.add),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            Image.network(currentUser.user.profilePic),
-            ListTile(
-              title: Text(
-                "Hello ${currentUser.user.name}",
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            ListTile(
-              title: const Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const SettingsPage()));
-              },
-            ),
-            ListTile(
-              title: const Text('Sign Out'),
-              onTap: () async {
-                await _auth.signOut();
-                ref.read(usersProvider.notifier).logOut();
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
--- 30_tw_writing_a_tweet/lib/providers/tweet_provider.dart
+++ 30_tw_writing_a_tweet/lib/providers/tweet_provider.dart
@@ -1,26 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:context/controllers/user_state.dart';
 import 'package:context/models/tweet.dart';
+import 'package:context/providers/user_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final tweetProvider = Provider<TweetApi>((ref) {
-  return TweetApi(ref);
+final tweetProvider = Provider<TwitterApi>((ref) {
+  return TwitterApi(ref);
 });
 
-class TweetApi {
-  TweetApi(this.ref);
+class TwitterApi {
+  TwitterApi(this.ref);
   final Ref ref;
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   Future<void> postTweet(String tweet) async {
-    LocalUser currentUser = ref.read(usersProvider);
-    await _firestore.collection('tweets').add(Tweet(
+    LocalUser currentUser = ref.read(userProvider);
+    await _firestore.collection('tweets').add(
+          Tweet(
             uid: currentUser.id,
-            name: currentUser.user.name,
             profilePic: currentUser.user.profilePic,
+            name: currentUser.user.name,
             tweet: tweet,
-            postTime: Timestamp.now())
-        .toMap());
+            postTime: Timestamp.now(),
+          ).toMap(),
+        );
   }
 }
